bf90aa326a25bd79c7c4886567dfb6db03cb982c
Ulrik Vander <ulrik@vander.dk>
2025-05-28 14:07:52 +0200

    Update vanderweb-bs4-accordion.php

--- vanderweb-bs4-accordion.php
+++ vanderweb-bs4-accordion.php
@@ -1,7 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
  *
  * This file is read by WordPress to generate the plugin information in the plugin
  * admin area. This file also includes all of the dependencies used by the plugin,
@@ -34,13 +33,13 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'VANDERWEB_BS4_ACCORDION_VERSION', '1.0.0' );
+define( 'VANDERWEB_CONTENT_VERSION', '0.0.1' );
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-vanderweb-bs4-accordion-activator.php
  */
-function activate_vanderweb_bs4_accordion() {
+function activate_vanderweb_content() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-vanderweb-bs4-accordion-activator.php';
 	Vanderweb_Bs4_Accordion_Activator::activate();
 }
@@ -49,13 +48,13 @@ function activate_vanderweb_bs4_accordion() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-vanderweb-bs4-accordion-deactivator.php
  */
-function deactivate_vanderweb_bs4_accordion() {
+function deactivate_vanderweb_content() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-vanderweb-bs4-accordion-deactivator.php';
 	Vanderweb_Bs4_Accordion_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_vanderweb_bs4_accordion' );
-register_deactivation_hook( __FILE__, 'deactivate_vanderweb_bs4_accordion' );
+register_activation_hook( __FILE__, 'activate_vanderweb_content' );
+register_deactivation_hook( __FILE__, 'deactivate_vanderweb_content' );
 
 /**
  * The core plugin class that is used to define internationalization,
