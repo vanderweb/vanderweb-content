422d23b97a41b5d766ec99dc6bcdfdac0e6787aa
Ulrik Vander <ulrik@vander.dk>
2025-05-28 14:28:37 +0200

    Update class-vanderweb-content-admin.php

--- admin/class-vanderweb-content-admin.php
+++ admin/class-vanderweb-content-admin.php
@@ -16,11 +16,11 @@
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the admin-specific stylesheet and JavaScript.
  *
- * @package    Vanderweb_Bs4_Accordion
- * @subpackage Vanderweb_Bs4_Accordion/admin
- * @author     Ulrik Vander <ulrik@vanderweb.com>
+ * @package    Vanderweb_Content
+ * @subpackage Vanderweb_Content/admin
+ * @author     Ulrik Vander <ulrik@vander.dk>
  */
-class Vanderweb_Bs4_Accordion_Admin {
+class Vanderweb_Content_Admin {
 
 	/**
 	 * The ID of this plugin.
@@ -40,7 +40,7 @@ class Vanderweb_Bs4_Accordion_Admin {
 	 */
 	private $version;
 
-	private $vanderweb_accordions_shortcodes_options;
+	private $vanderweb_content_shortcodes_options;
 	
 	/**
 	 * Initialize the class and set its properties.
@@ -54,7 +54,7 @@ class Vanderweb_Bs4_Accordion_Admin {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 		
-		add_action( 'admin_init', array( $this, 'vanderweb_accordions_shortcodes_page_init' ) );
+		add_action( 'admin_init', array( $this, 'vanderweb_content_shortcodes_page_init' ) );
 
 	}
 
@@ -77,7 +77,7 @@ class Vanderweb_Bs4_Accordion_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/vanderweb-bs4-accordion-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/vanderweb-content-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -100,7 +100,7 @@ class Vanderweb_Bs4_Accordion_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/vanderweb-bs4-accordion-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/vanderweb-content-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
@@ -125,12 +125,12 @@ class Vanderweb_Bs4_Accordion_Admin {
 		//add_submenu_page( 'plugins.php', 'Plugin settings page title', 'Admin area menu slug', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page' ) );
 
 		add_submenu_page(
-			'edit.php?post_type=vanderweb_accordions',
-			__( 'Accordions - Shortcodes', 'vanderweb-bs4-accordion' ),
-			__( 'Shortcodes', 'vanderweb-bs4-accordion' ),
+			'edit.php?post_type=vanderweb_content',
+			__( 'content - Shortcodes', 'vanderweb-content' ),
+			__( 'Shortcodes', 'vanderweb-content' ),
 			'manage_options',
-			'vanderweb-accordions-shortcodes',
-			array($this, 'vanderweb_accordions_shortcodes' )
+			'vanderweb-content-shortcodes',
+			array($this, 'vanderweb_content_shortcodes' )
 		);
 
 	}
@@ -139,43 +139,43 @@ class Vanderweb_Bs4_Accordion_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function vanderweb_accordions_shortcodes() {
-		$this->vanderweb_accordions_shortcodes_options = get_option( 'vanderweb_accordions_shortcodes_option_name' ); 
+	public function vanderweb_content_shortcodes() {
+		$this->vanderweb_content_shortcodes_options = get_option( 'vanderweb_content_shortcodes_option_name' ); 
 		include_once( 'partials/' . $this->plugin_name . '-admin-display.php' );
 
 	}
-	public function vanderweb_accordions_shortcodes_page_init() {
+	public function vanderweb_content_shortcodes_page_init() {
 		register_setting(
-			'vanderweb_accordions_shortcodes_option_group', // option_group
-			'vanderweb_accordions_shortcodes_option_name', // option_name
-			array( $this, 'vanderweb_accordions_shortcodes_sanitize' ) // sanitize_callback
+			'vanderweb_content_shortcodes_option_group', // option_group
+			'vanderweb_content_shortcodes_option_name', // option_name
+			array( $this, 'vanderweb_content_shortcodes_sanitize' ) // sanitize_callback
 		);
 
 		add_settings_section(
-			'vanderweb_accordions_shortcodes_setting_section', // id
-			__('Settings:', 'vanderweb-bs4-accordion'), // title
-			array( $this, 'vanderweb_accordions_shortcodes_section_info' ), // callback
-			'vanderweb-accordions-shortcodes-admin' // page
+			'vanderweb_content_shortcodes_setting_section', // id
+			__('Settings:', 'vanderweb-content'), // title
+			array( $this, 'vanderweb_content_shortcodes_section_info' ), // callback
+			'vanderweb-content-shortcodes-admin' // page
 		);
 
 		add_settings_field(
 			'load_bootstrap_4_6_from_the_plugin_0', // id
-			__('Load Bootstrap 4.6 from the plugin', 'vanderweb-bs4-accordion'), // title
+			__('Load Bootstrap 4.6 from the plugin', 'vanderweb-content'), // title
 			array( $this, 'load_bootstrap_4_6_from_the_plugin_0_callback' ), // callback
-			'vanderweb-accordions-shortcodes-admin', // page
-			'vanderweb_accordions_shortcodes_setting_section' // section
+			'vanderweb-content-shortcodes-admin', // page
+			'vanderweb_content_shortcodes_setting_section' // section
 		);
 
 		add_settings_field(
 			'load_bootstrap_icons_1_2_from_the_plugin_1', // id
-			__('Load Bootstrap Icons 1.2 from the plugin', 'vanderweb-bs4-accordion'), // title
+			__('Load Bootstrap Icons 1.2 from the plugin', 'vanderweb-content'), // title
 			array( $this, 'load_bootstrap_icons_1_2_from_the_plugin_1_callback' ), // callback
-			'vanderweb-accordions-shortcodes-admin', // page
-			'vanderweb_accordions_shortcodes_setting_section' // section
+			'vanderweb-content-shortcodes-admin', // page
+			'vanderweb_content_shortcodes_setting_section' // section
 		);
 	}
 
-	public function vanderweb_accordions_shortcodes_sanitize($input) {
+	public function vanderweb_content_shortcodes_sanitize($input) {
 		$sanitary_values = array();
 		if ( isset( $input['load_bootstrap_4_6_from_the_plugin_0'] ) ) {
 			$sanitary_values['load_bootstrap_4_6_from_the_plugin_0'] = $input['load_bootstrap_4_6_from_the_plugin_0'];
@@ -188,27 +188,27 @@ class Vanderweb_Bs4_Accordion_Admin {
 		return $sanitary_values;
 	}
 
-	public function vanderweb_accordions_shortcodes_section_info() {
+	public function vanderweb_content_shortcodes_section_info() {
 		?>
-		<p><?php echo __( 'These Shortcodes require that Bootstrap 4.6 and Bootstrap Icons 1.2 is loaded to work.', 'vanderweb-bs4-accordion' ); ?></p>
-		<p><?php echo __( 'If your current theme loads Bootstrap 4.6 and Bootstrap Icons 1.2, then set options to "No".', 'vanderweb-bs4-accordion' ); ?></p>
-		<p><?php echo __( 'Otherwise change the settings to load from this plugin.', 'vanderweb-bs4-accordion' ); ?></p>
+		<p><?php echo __( 'These Shortcodes require that Bootstrap 4.6 and Bootstrap Icons 1.2 is loaded to work.', 'vanderweb-content' ); ?></p>
+		<p><?php echo __( 'If your current theme loads Bootstrap 4.6 and Bootstrap Icons 1.2, then set options to "No".', 'vanderweb-content' ); ?></p>
+		<p><?php echo __( 'Otherwise change the settings to load from this plugin.', 'vanderweb-content' ); ?></p>
 		<?php
 	}
 
 	public function load_bootstrap_4_6_from_the_plugin_0_callback() {
-		?> <fieldset><?php $checked = ( $this->vanderweb_accordions_shortcodes_options['load_bootstrap_4_6_from_the_plugin_0'] === 'true' ) ? 'checked' : '' ; ?>
-		<label for="load_bootstrap_4_6_from_the_plugin_0-0"><input type="radio" name="vanderweb_accordions_shortcodes_option_name[load_bootstrap_4_6_from_the_plugin_0]" id="load_bootstrap_4_6_from_the_plugin_0-0" value="true" <?php echo $checked; ?>> <?php echo __('Yes', 'vanderweb-bs4-accordion'); ?></label><br>
-		<?php $checked = ( isset( $this->vanderweb_accordions_shortcodes_options['load_bootstrap_4_6_from_the_plugin_0'] ) && $this->vanderweb_accordions_shortcodes_options['load_bootstrap_4_6_from_the_plugin_0'] === 'false' ) ? 'checked' : '' ; ?>
-		<label for="load_bootstrap_4_6_from_the_plugin_0-1"><input type="radio" name="vanderweb_accordions_shortcodes_option_name[load_bootstrap_4_6_from_the_plugin_0]" id="load_bootstrap_4_6_from_the_plugin_0-1" value="false" <?php echo $checked; ?>> <?php echo __('No, load Bootstrap 4.6 from your theme', 'vanderweb-bs4-accordion'); ?></label></fieldset> 
+		?> <fieldset><?php $checked = ( $this->vanderweb_content_shortcodes_options['load_bootstrap_4_6_from_the_plugin_0'] === 'true' ) ? 'checked' : '' ; ?>
+		<label for="load_bootstrap_4_6_from_the_plugin_0-0"><input type="radio" name="vanderweb_content_shortcodes_option_name[load_bootstrap_4_6_from_the_plugin_0]" id="load_bootstrap_4_6_from_the_plugin_0-0" value="true" <?php echo $checked; ?>> <?php echo __('Yes', 'vanderweb-bs4-accordion'); ?></label><br>
+		<?php $checked = ( isset( $this->vanderweb_content_shortcodes_options['load_bootstrap_4_6_from_the_plugin_0'] ) && $this->vanderweb_content_shortcodes_options['load_bootstrap_4_6_from_the_plugin_0'] === 'false' ) ? 'checked' : '' ; ?>
+		<label for="load_bootstrap_4_6_from_the_plugin_0-1"><input type="radio" name="vanderweb_content_shortcodes_option_name[load_bootstrap_4_6_from_the_plugin_0]" id="load_bootstrap_4_6_from_the_plugin_0-1" value="false" <?php echo $checked; ?>> <?php echo __('No, load Bootstrap 4.6 from your theme', 'vanderweb-bs4-accordion'); ?></label></fieldset> 
 		<?php
 	}
 
 	public function load_bootstrap_icons_1_2_from_the_plugin_1_callback() {
-		?> <fieldset><?php $checked = ( $this->vanderweb_accordions_shortcodes_options['load_bootstrap_icons_1_2_from_the_plugin_1'] === 'true' ) ? 'checked' : '' ; ?>
-		<label for="load_bootstrap_icons_1_2_from_the_plugin_1-0"><input type="radio" name="vanderweb_accordions_shortcodes_option_name[load_bootstrap_icons_1_2_from_the_plugin_1]" id="load_bootstrap_icons_1_2_from_the_plugin_1-0" value="true" <?php echo $checked; ?>> <?php echo __('Yes', 'vanderweb-bs4-accordion'); ?></label><br>
-		<?php $checked = ( isset( $this->vanderweb_accordions_shortcodes_options['load_bootstrap_icons_1_2_from_the_plugin_1'] ) && $this->vanderweb_accordions_shortcodes_options['load_bootstrap_icons_1_2_from_the_plugin_1'] === 'false' ) ? 'checked' : '' ; ?>
-		<label for="load_bootstrap_icons_1_2_from_the_plugin_1-1"><input type="radio" name="vanderweb_accordions_shortcodes_option_name[load_bootstrap_icons_1_2_from_the_plugin_1]" id="load_bootstrap_icons_1_2_from_the_plugin_1-1" value="false" <?php echo $checked; ?>> <?php echo __('No, load Bootstrap Icons 1.2 from your theme', 'vanderweb-bs4-accordion'); ?></label></fieldset> 
+		?> <fieldset><?php $checked = ( $this->vanderweb_content_shortcodes_options['load_bootstrap_icons_1_2_from_the_plugin_1'] === 'true' ) ? 'checked' : '' ; ?>
+		<label for="load_bootstrap_icons_1_2_from_the_plugin_1-0"><input type="radio" name="vanderweb_content_shortcodes_option_name[load_bootstrap_icons_1_2_from_the_plugin_1]" id="load_bootstrap_icons_1_2_from_the_plugin_1-0" value="true" <?php echo $checked; ?>> <?php echo __('Yes', 'vanderweb-bs4-accordion'); ?></label><br>
+		<?php $checked = ( isset( $this->vanderweb_content_shortcodes_options['load_bootstrap_icons_1_2_from_the_plugin_1'] ) && $this->vanderweb_content_shortcodes_options['load_bootstrap_icons_1_2_from_the_plugin_1'] === 'false' ) ? 'checked' : '' ; ?>
+		<label for="load_bootstrap_icons_1_2_from_the_plugin_1-1"><input type="radio" name="vanderweb_content_shortcodes_option_name[load_bootstrap_icons_1_2_from_the_plugin_1]" id="load_bootstrap_icons_1_2_from_the_plugin_1-1" value="false" <?php echo $checked; ?>> <?php echo __('No, load Bootstrap Icons 1.2 from your theme', 'vanderweb-bs4-accordion'); ?></label></fieldset> 
 		<?php
 	}
 }
